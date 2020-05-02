17ac8cd7d3f69f321489e90b0b70a8df57e5c2ca
HillaPeter <peterhilla@gmail.com>
2020-05-02 18:27:11 +0300

    about-section

--- About.js
+++ About.js
@@ -1,34 +1,42 @@
-var modal = document.getElementById("aboutSection");
+var modal = document.getElementById('aboutSection');
 
+// Get the button that opens the modal
+var btn = document.getElementById("aboutMenu");
+
+// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
-    modal.style.display = "none";
-    document.getElementById("aboutSection").close();
-   // showMenuWelcome();
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    document.getElementById('aboutSection').style.display='block';
+  //  showPrimary();
 }
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    document.getElementById('aboutSection').style.display='none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
-        document.getElementById("aboutSection").close();
-      //  showMenuWelcome();
+        alert("s");
+        document.getElementById('aboutSection').style.display='none';
     }
 }
 
+window.onkeydown = function( event ){
+    // ESCAPE key pressed
+    if ( event.keyCode == 27) {
+        document.getElementById('aboutSection').style.display='none';
+    }
+};
 
 function showModal(){
     if (!$('#aboutSection').is(':visible')) {
-        document.getElementById("aboutSection").close();
+        document.getElementById('aboutSection').style.display='none';
     }else{
-        document.getElementById("aboutSection").showModal();
+        document.getElementById('aboutSection').style.display='block';
     }
 
-}
-
-
-function closeAbout(){
-   // $('#aboutSection').dialog("close");
-     document.getElementById("aboutSection").close();
-    showMenuWelcome();
 }
